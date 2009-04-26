68aaa20850df9e8ea1b3c871fd8c20128c9e8b07
unknown <igus@.(none)>
2009-04-26 11:53:02 +0100

    Added the method source to firefox.rb     # Description:     #   Return the source from the page, whether the source includes html tags or not     #     # Output:     #   Full source of the page     #   Html tags will be returnd in capitols eg. <TABLE id='abc'> </TABLE>     #   Attribues and their values will be returned in the case they are set in.     #   The output will always be wrapped in valid HTML tags, whether they are present or not.
    Added source1.html and source2.html as well as source_test.rb to provide test cases for source

---
+++ firewatir/unittests/source_test.rb
@@ -0,0 +1,22 @@
+# feature tests for Source
+# revision: $Revision: 1.0 $
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') unless $SETUP_LOADED
+require 'unittests/setup'
+
+class TC_Source < Test::Unit::TestCase
+
+  def test_Get_HTML_SOURCE
+    goto_page("source1.html")
+    @source1 = browser.source
+    @check1 ="This is a html tagged page, its great.\n<B> Some bold letters </B>"
+    assert_match(@check1, @source1)
+  end
+
+  def test_GET_NON_HTML_SOURCE
+    goto_page("source2.html")
+    @source2= browser.source
+    @check2 = "<TAG html=\"not\">\nThis is a not html tagged page.\n</TAG>"
+    assert_match(@check2, @source2)
+  end
+end
