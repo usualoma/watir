8a90fef8cea0f090312a0f3f8588d615c7876e8b
Alex Collins <alex@ajcollins.com>
2009-05-17 22:13:50 +0100

    Added details around how to get this test to pass.

--- firewatir/unittests/multiple_browsers_test.rb
+++ firewatir/unittests/multiple_browsers_test.rb
@@ -12,6 +12,15 @@ class TC_MultipleBrowsers < Test::Unit::TestCase
     end
 
     def test_multiple_browsers
+      #
+      # This test relies upon you having version 0.95 of the JSSH plugin installed.
+      # This can be installed from the plugins folder in: http://github.com/ajcollins/JSSH-XPIs/tree/master
+      # 
+      # You need to create a profile named 'port9998' for this to work, 
+      # and then install the JSSH XPI into this profile.
+      #
+      # You will also need TCP port 9998 to be unused by anything else on your system.
+      #
       options = { :multiple_browser_xpi => true, :port => 9998, :profile => 'port9998' }
       browser2 = FireWatir::Firefox.new(options)
       
